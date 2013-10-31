c40b9bac94917d9f3e5cd35d8e1b90a91141d4b9
Dan LaMotte <lamotte85@gmail.com>
2013-10-31 11:51:31 -0500

    js/bbks: opt-in model caching, no_conflict()

--- krankshaft/static/js/backbone-krankshaft.js
+++ krankshaft/static/js/backbone-krankshaft.js
@@ -34,6 +34,36 @@
         return opts;
       },
       username: ''
+    },
+    cache: {
+      add: function(inst) {
+        bb.ks.get_or_create_collection(inst.constructor)
+              .set([inst], {remove: false});
+      },
+      cached: [],
+      get: function(model, id) {
+        return
+          bb.ks.get_or_create_collection(model)
+                .get(id);
+      },
+      get_or_create_collection: function(model) {
+        var collection = _.find(bb.ks.cache.cached, function(collection) {
+          return collection.model == model;
+        });
+
+        if (! collection) {
+          collection = new bb.Collection();
+          collection.model = model;
+          bb.ks.cache.cached.push(collection);
+        }
+
+        return collection;
+      }
+    },
+    no_conflict: function() {
+      bb.sync = bb.sync_ks_old;
+      bb.Model.prototype.fetch = bb.Model.prototype.fetch_ks_old;
+      bb.Collection.prototype.fetch = bb.Collection.prototype.fetch_ks_old;
     }
   };
 
@@ -46,5 +76,34 @@
     }
 
     return bb.ks_old_sync(method, model, update(opts));
+  };
+
+  bb.Model.prototype.fetch_ks_old = bb.Model.prototype.fetch;
+  bb.Model.prototype.fetch_ks = bb.Model.prototype.fetch = function(opts) {
+    var me = this;
+    var request = bb.Model.prototype.fetch_ks_old.call(this, opts);
+
+    if (this.constructor.cached === true) {
+      request.done(function() {
+        bb.ks.cache.add(me);
+      });
+    }
+
+    return request;
+  };
+
+  bb.Collection.prototype.fetch_ks_old = bb.Collection.prototype.fetch;
+  bb.Collection.prototype.fetch_ks = bb.Collection.prototype.fetch = function(opts) {
+    var me = this;
+    var request = bb.Collection.prototype.fetch_ks_old.call(this, opts);
+
+    if (this.model.cached === true) {
+      request.done(function() {
+        bb.ks.cache.get_or_create_collection(me.model)
+          .set(me.models, {remove: false});
+      });
+    }
+
+    return request;
   };
 }(jQuery, Backbone, _));
