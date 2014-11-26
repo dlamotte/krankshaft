6a56fa2e1fb39b75e795f1c0f754ca814dd25422
Dan LaMotte <lamotte85@gmail.com>
2014-11-26 10:50:29 -0600

    v0.3.12

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except IOError:
 
 setup(
     name='krankshaft',
-    version='0.3.11',
+    version='0.3.12',
     author='Dan LaMotte',
     author_email='lamotte85@gmail.com',
     url='https://github.com/dlamotte/krankshaft',
